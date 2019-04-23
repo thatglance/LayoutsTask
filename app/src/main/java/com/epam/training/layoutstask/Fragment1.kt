10cf9ff7d1330a751df6e3b911925042296eec85
thatglance <11sashamak@gmail.com>
2019-04-23 22:49:41 +0300

    ready task

---
+++ app/src/main/java/com/epam/training/layoutstask/Fragment1.kt
@@ -0,0 +1,51 @@
+package com.epam.training.layoutstask
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment1.*
+
+/**
+ * [Fragment1] is based on ConstraintLayout. Shows a toast message at a floating action button click.
+ *
+ * @author Alexandra Makovskaya
+ */
+class Fragment1 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment1, container, false)
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener{
+            Toast.makeText(activity, R.string.fragment_1_toast, Toast.LENGTH_LONG).show()
+        }
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        departureDateTextView.setText(R.string.default_departure_date)
+        val departureFreeSeatsText = getString(R.string.free_seats_label) + " " + getString(R.string.default_departure_free_seats)
+        freeSeatsDeparture.text = departureFreeSeatsText
+        departureCostTextView.setText(R.string.default_cost_departure)
+        departureFrom.setText(R.string.default_departure_from)
+        departureTo.setText(R.string.default_departure_to)
+        departureLeaveTime.setText(R.string.default_departure_time_from)
+        departureArriveTime.setText(R.string.default_departure_time_to)
+        departureFlightTime.setText(R.string.default_departure_time_flight)
+
+        returnDateTextView.setText(R.string.default_return_date)
+        val returnFreeSeatsText = getString(R.string.free_seats_label) + " " + getString(R.string.default_return_free_seats)
+        freeSeatsReturn.text = returnFreeSeatsText
+        returnCostTextView.setText(R.string.default_cost_return)
+        returnFrom.setText(R.string.default_return_from)
+        returnTo.setText(R.string.default_return_to)
+        returnLeaveTime.setText(R.string.default_return_time_from)
+        returnArriveTime.setText(R.string.default_return_time_to)
+        returnFlightTime.setText(R.string.default_return_time_flight)
+    }
+}
--- app/src/main/java/com/epam/training/layoutstask/MainActivity.kt
+++ app/src/main/java/com/epam/training/layoutstask/MainActivity.kt
@@ -2,11 +2,35 @@ package com.epam.training.layoutstask
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.AppCompatButton
 
+/**
+ * Main activity of application.
+ * [MainActivity] contains 2 buttons to show [Fragment1] or [Fragment2].
+ *
+ * @author Alexandra Makovskaya
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,
+                Fragment.instantiate(this, Fragment1::class.java.canonicalName)).commit()
+        }
+
+        val btnFragment1 = findViewById<AppCompatButton>(R.id.buttonFragment1)
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, Fragment1()).commit()
+        }
+
+        val btnFragment2 = findViewById<AppCompatButton>(R.id.buttonFragment2)
+        btnFragment2.setOnClickListener{
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, Fragment2()).commit()
+        }
     }
+
 }
